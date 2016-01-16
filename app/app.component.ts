6b49bb621d2552286e17748406195afe27eedf7d
yaocl <yaochenglindlut@gmail.com>
2016-01-16 23:22:50 +0800

    first kick

---
+++ app/app.component.ts
@@ -0,0 +1,87 @@
+import {Component} from 'angular2/core';
+import {Hero} from './Hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HEROES} from './mock-heroes';
+import {HeroService} from './hero.service';
+import {OnInit} from 'angular2/core';
+import { HTTP_PROVIDERS, Http} from 'angular2/http';
+
+@Component({
+  selector: 'my-app',
+  template:`
+    <h1>{{title}}</h1>
+    <ul class="heroes">
+     <li *ngFor="#hero of heroes" (click)="onSelect(hero)" [class.selected]="hero ===selectedHero">
+         <span class="badge">{{hero.id}}</span> {{hero.name}}
+     </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    `,
+    styles:[`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 10em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0em;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #EEE;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0em 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0px 0px 4px;
+  }
+`],
+directives: [HeroDetailComponent],
+providers: [HeroService, HTTP_PROVIDERS]
+})
+export class AppComponent implements OnInit {
+  ngOnInit(){this.getHeroes()}
+  constructor(private _heroService: HeroService){}
+  public title = 'Tour of Heroes';
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  public heroes = HEROES;
+  public selectedHero: Hero;
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
+  };
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+}
